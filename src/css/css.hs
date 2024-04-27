3c9b568e91135895af21f08b15d60265bac7d62a
silky <noonsilk+-noonsilk@gmail.com>
2024-04-27 13:12:42 +0100

    Initial

---
+++ src/css/css.hs
@@ -0,0 +1,364 @@
+{-# language OverloadedStrings #-}
+
+import Clay
+import Control.Monad  ( forM_ )
+import Prelude hiding
+ ( span
+ , div
+ , rem
+ )
+import qualified Data.Text  as Text
+import qualified Clay.Media as Media
+import Clay.Flexbox  qualified as Flex
+
+-- ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+--
+-- Helpful things
+
+allMargin n       = margin  n n n n
+allPadding n      = padding n n n n
+allBorderRadius n = borderRadius n n n n
+
+margin0  = margin  (px 0) (px 0) (px 0) (px 0)
+padding0 = padding (px 0) (px 0) (px 0) (px 0)
+
+coreTextFont = fontFamily ["Work Sans"] [sansSerif]
+monoFont     = fontFamily ["Fira Code"] [monospace]
+titleFont    = fontFamily ["Literata"]  [sansSerif]
+menuFont     = coreTextFont
+
+bgColour :: Color
+bgColour = "#fffbf0"
+
+selectionColour :: Color
+selectionColour = "#eaeaea"
+
+textColour :: Color
+textColour = "#383838"
+
+softTextColour :: Color
+softTextColour = "#989888"
+
+linkColour :: Color
+-- linkColour = "#c832ff"
+linkColour = "#9a99dd"
+
+otherStrongColour :: Color
+otherStrongColour = "#6232ff"
+
+linkSoftColour :: Color
+linkSoftColour = bgColour
+
+commentColour :: Color
+commentColour = otherStrongColour
+
+withSmallDisplay = query Clay.all [Media.maxWidth 1200]
+withPhoneDisplay = query Clay.all [Media.maxWidth  450]
+
+replaced = "#dd99dc"
+removed  = "#dd9a99"
+added    = "#99dd9a"
+
+-- ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+
+main :: IO ()
+main = putCss
+  $  basics
+  >> fonts
+  >> headings
+  >> offerings
+  >> centralLayout
+  >> guides
+  >> blog
+
+
+blog :: Css
+blog = do
+  div # ".post" |> div # ".top" |> div # ".image" ? do
+    height $ px 600
+
+  div # ".image" ? do
+    height $ px 400
+    backgroundColor bgColour
+    backgroundSize cover
+    allBorderRadius $ px 20
+    "background-position"   -: "center"
+    "background-blend-mode" -: "multiply"
+    "box-shadow" -: "4.7px 9.3px 9.3px hsl(0deg 0% 0% / 0.15);"
+
+  div # ".content" ? do
+    marginTop $ px 20
+  --   alignItems center
+  --   flexDirection column
+  --   div # ".summary" ? do
+  --     fontStyle italic
+  --     width $ pct 80
+      -- backgroundColor ("#eaeaea")
+      -- fontSize $ em 1.2
+
+  section # ".posts" ? do
+    display flex
+    div # ".post" ? do
+
+      display flex
+      flexDirection column
+      width (px 1000)
+
+      h3 ? do
+        a ? do
+          fontSize $ em 0.7
+
+
+guides :: Css
+guides = do
+  div # ".guides" ? do
+    display flex
+    flexDirection row
+    flexWrap Flex.wrap
+    alignItems center
+    justifyContent center
+
+    let mkGuide sel c =
+          div # sel ? do
+            border (px 1) solid (lighten 0.0 c)
+            h4 ? (backgroundColor $ lighten 0.74 c)
+            div # ".img" ? do
+              backgroundColor $ lighten 0.74 c
+              span ? background (lighten 0.0 c)
+            div # ".link" ? do
+              a ? do
+                backgroundColor (lighten 0.3 c)
+                hover & backgroundColor (lighten 0.6 c)
+
+    mkGuide ".guide.one"   linkColour
+    mkGuide ".guide.two"   added
+    mkGuide ".guide.three" replaced
+
+    div # ".guide" ? do
+      display flex
+      flexDirection column
+      width  $ px 500
+      allMargin $ px 30
+      allBorderRadius $ px 20
+      "box-shadow" -: "4.7px 9.3px 9.3px hsl(0deg 0% 0% / 0.15);"
+
+      div # ".img" ? do
+        borderRadius (px 20) (px 20) 0 0
+        marginBottom $ px 0
+        paddingTop    $ px 30
+        paddingBottom $ px 20
+        span ? do
+          allMargin $ px 20
+          marginBottom $ px 0
+          allPadding $ px 10
+          allBorderRadius $ px 10
+
+      div ? do
+        margin0
+
+      p ? do
+        allMargin $ px 20
+
+      h4 ? do
+        margin0
+        allPadding $ px 10
+        titleFont
+        fontSize $ em 1.4
+
+      div # ".link" ? do
+        marginBottom $ px 20
+        display flex
+        justifyContent center
+        alignItems center
+        a ? do
+          color black
+          allPadding      $ px 10
+          allBorderRadius $ px 10
+
+
+centralLayout :: Css
+centralLayout = do
+  div # ".links" ? do
+    display flex
+    flexDirection row
+    allMargin $ px 20
+    a ? do
+      marginLeft  $ px 10
+      marginRight $ px 10
+
+  div # ".banner" ? do
+    backgroundColor bgColour
+    allPadding (px 20)
+
+  footer ? do
+    alignItems center
+    color softTextColour
+    fontSize (em 0.8)
+
+  code ? do
+    monoFont
+    fontSize (em 0.9)
+
+  pre ? do
+    monoFont
+    paddingTop $ px 15
+    paddingBottom $ px 15
+    lineHeight $ px 40
+    marginLeft (px 10)
+    borderLeft (px 7) solid ("#eaeaea" :: Color)
+    backgroundColor ("#fafafa" :: Color)
+    paddingLeft $ px 10
+    whiteSpace preWrap
+
+    span # ".comment" ? do
+      color commentColour
+
+
+offerings :: Css
+offerings = do
+  div # ".offerings" ? do
+    allMargin (px 30)
+    display flex
+    flexDirection column
+    alignItems center
+
+    withPhoneDisplay $ do
+      allMargin (px 10)
+
+    div # ".offering" ? do
+      display flex
+      flexDirection column
+      maxWidth (pct 80)
+
+      query Clay.all [Media.maxWidth 1600] $ do
+        flexDirection column
+        maxWidth (pct 100)
+
+      justifyContent center
+
+      div # ".product-image" ? do
+        display flex
+        flexDirection row
+        paddingRight (px 20)
+        alignItems center
+        justifyContent center
+        marginBottom (px 20)
+
+        query Clay.all [Media.maxWidth 1400] $ do
+          flexDirection column
+
+        img ? do
+          backgroundColor bgColour
+          allMargin (px 10)
+          border (px 1) solid black
+          allBorderRadius (px 20)
+          "box-shadow" -: "4.7px 9.3px 9.3px hsl(0deg 0% 0% / 0.15);"
+          withPhoneDisplay $ do
+            width $ px 400
+
+      div # ".content" ? do
+        marginLeft (px 20)
+
+
+headings :: Css
+headings = do
+  h1 <> h2 <> h3 <> h4 <> h5 ? do
+    allMargin (px 0)
+    titleFont
+
+
+  h2 |> span # ".beta" ? do
+    allBorderRadius (px 5)
+    allMargin (px 8)
+    allPadding (px 2)
+    background linkColour
+    color white
+    fontSize (em 0.5)
+    monoFont
+    paddingLeft (px 5)
+    paddingRight (px 5)
+    "box-shadow" -: "3.7px 4.3px 4.3px hsl(0deg 0% 0% / 0.15);"
+
+  div # ".title" ? do
+    marginBottom $ px 20
+    withPhoneDisplay $ do
+      fontSize $ em 0.8
+
+    h2 ? do
+      fontSize $ em 2.2
+
+    h3 ? do
+      fontSize     $ em 1.9
+      marginTop    $ px 40
+      marginBottom $ px 40
+      fontWeight   normal
+      border (px 0) solid black
+      color linkColour
+
+    h4 ? do
+      fontWeight normal
+      fontSize $ em 1.4
+      lineHeight $ em 1.8
+      marginBottom $ px 30
+
+
+  div # ".top" ? do
+    display flex
+    flexDirection column
+
+  h4 ? do
+    marginTop (px 20)
+    coreTextFont
+
+  h3 ? do
+    borderBottom (px 1) solid otherStrongColour
+
+  h1 # ".banner" ? do
+    fontSize $ em 1.6
+    small ? do
+      fontSize $ em 0.6
+      fontStyle italic
+      fontWeight normal
+      color softTextColour
+
+
+basics :: Css
+basics = do
+  let elts
+        =  body
+        <> section
+        <> footer
+        <> nav
+
+  elts ? do
+    display flex
+    flexDirection column
+    margin0
+    padding0
+
+  p <> li <> pre ? do
+    lineHeight (px 40)
+
+  a ? do
+    color black
+
+  a # hover ? do
+    fontStyle italic
+
+  let selectionStyle =
+        do
+          background selectionColour
+          color      black
+
+  selection          & selectionStyle
+  "::-moz-selection" & selectionStyle
+
+
+fonts :: Css
+fonts = do
+  body ? do
+    coreTextFont
+    fontSize (px 26)
+    color textColour
+
---
+++ src/guides/getting-started-with-metadelta.md
@@ -0,0 +1,187 @@
+---
+previewImage: /images/blog/github-action.webp
+title: "Getting started with Metadelta &mdash; Setting up a GitHub action"
+authors:
+ - Noon van der Silk
+date: 2024-02-24
+tags:
+  - github
+  - metadelta
+summary: |
+  If you've had a look at the demos and your keen to see how Metadelta looks
+  with your own database, probably the easiest way is just to set up the
+  GitHub action integration! Let's go over how to implement this step-by-step.
+---
+
+## Motivation
+
+Metadelta's main function is to compute _differences_ between different Hasura
+versions. The most natural place to compute such a difference is, if you use
+GitHub, during a Pull Request (PR): You compare the permission changes that a
+PR seeks to introduce.
+
+This, then, is what we will implement in what follows: We will set up an
+action that comments with a link to the Metadelta UI, showing the diff of that
+particular PR. A new link will be provided each time the PR branch is updated.
+
+## The setup
+
+The essential requirement is that you have in your repo somewhere a `hasura`
+folder with something like the following structure:
+
+```shell
+./hasura/metadata
+  - actions.graphql
+  - actions.yaml
+  ...
+  databases/
+  ...
+  - version.yaml
+```
+
+If you'd like to follow along with our repo instead of your own, you can start
+with our [demo-database](https://github.com/InvariantClub/demo-database) which
+has everything you need.
+
+## The details
+
+So, the set up is pretty straightfoward. If you just want to view the finished
+[GitHub workflow
+file](https://github.com/InvariantClub/demo-database/blob/main/.github/workflows/compute-permission-diff.yaml)
+you can; but otherwise, we'll go through all the steps.
+
+**The header**
+
+We start with a simple workflow that runs on pull requests. We require two
+kinds of permissions: we need to write `contents` as we're going to upload an
+artifact later, and we want to write to the pull-request to comment on it.
+
+```yaml
+name: Compute permission diff on PR
+
+on:
+  pull_request:
+
+
+jobs:
+  compute-diff:
+    permissions:
+      contents: write
+      pull-requests: write
+    runs-on: ubuntu-latest
+    steps:
+```
+
+The first steps:
+
+**1. Check out the two versions of the code**
+
+Pretty simple; we just check out the respective versions to an `/old` and
+`/new` path.
+
+```yaml
+      - name: Checkout the PR as the 'new' source
+        uses: actions/checkout@v4
+        with:
+          path: ./new
+          ref: ${{ github.event.pull_request.head.sha }}
+
+      - name: Checkout `main` as the 'old' source
+        uses: actions/checkout@v4
+        with:
+          path: ./old
+          ref: main
+```
+
+**2. Run Metadelta to produce a `diff.json` file**
+
+The juicy part; we run Metadelta, from <a
+href="https://github.com/orgs/InvariantClub/packages/container/package/metadelta-cli">our
+docker image</a>, referring to the `./old` and `./new` paths we checked out
+earlier.
+
+A few detalis:
+
+- We set some labels so that it displays nicely in the UI and is clear what
+commits it is referencing,
+- We compute a small check to test whether we _actually_ found a difference,
+  In subsequent steps this will be used to check if we upload an artifact and
+  post a comment on GitHub.
+
+```yaml
+      - name: Run metadelta comparing old and new
+        id: metadelta
+        run: |
+          docker run -i \
+            -v $PWD:/work \
+            ghcr.io/invariantclub/metadelta-cli \
+            diff \
+            -o /work/old/hasura/metadata/ \
+            --oldLabel 'main' \
+            -n /work/new/hasura/metadata/ \
+            --newLabel '${{ github.event.pull_request.head.label }} @ ${{ github.event.pull_request.head.sha }}' \
+            --newLink '${{ github.event.pull_request.html_url }}' \
+            >diff.json || true
+          echo "anythingDifferent=$(test -s diff.json || echo 'true')" >> "$GITHUB_OUTPUT"
+```
+
+**3. Upload the `diff.json` file as a GitHub artifact**
+
+Pretty straightforward: We are going to upload it so that it can be downloaded
+by the UI later on. The only thing to note is that we only do the upload if
+the previous step found a difference. In reality, it's probably far more
+common for PRs to _not_ be changing permissions; so it's important to skip any
+extra work in those cases.
+
+
+```yaml
+      - name: Uploading the diff as an artifact
+        uses: actions/upload-artifact@v4
+        id: artifact
+        if:
+          # Only run if we detected changes.
+          ${{ steps.metadelta.outputs.anythingDifferent != 'true' }}
+        with:
+          name: metadelta-diff
+          path: diff.json
+          retention-days: 5
+```
+
+**4. Comment on the PR with a link**
+
+This is the final step. The Metadelta UI can load a GitHub artifact completely
+client-side, (supposing you have configured the UI, in the [settings tab](https://metadelta.invariant.club/settings), with a GitHub access
+token, which is, unfortunately, [a requirement for querying GitHub artifacts,
+even public ones](https://docs.github.com/en/rest/actions/artifacts?apiVersion=2022-11-28#download-an-artifact)); so all we need to do is prepare an appropriate URL, and post
+that as a comment on the PR.
+
+As the above step was run conditionally, so too we want to run this one on the
+condition that the previous step was completed.
+
+```yaml
+      - name: Comment with Metadelta link
+        uses: peter-evans/create-or-update-comment@v4.0.0
+        if:
+          # Only run if we uploaded an artifact
+          ${{ steps.artifact.outcome == 'success' }}
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          issue-number: ${{ github.event.pull_request.number }}
+          body: |
+            [View the permission diff in Metadelta :eyes:](https://metadelta.invariant.club/explorer?githubArtifact=${{ steps.artifact.outputs.artifact-id }}/${{ github.repository_owner }}/${{ github.event.repository.name }})
+
+            This is was triggered from GitHub Actions [run ${{ github.run_number }}](https://github.com/${{ github.repository_owner }}/${{ github.event.repository.name }}/actions/runs/${{ github.run_id }}/).
+```
+
+
+## That's it!
+
+Putting that all together in [a single workflow file, perhaps called
+`compute-permission-diff.yaml`](https://github.com/InvariantClub/demo-database/blob/main/.github/workflows/compute-permission-diff.yaml)
+does the job!
+
+You are now computing diffs on all PRs, and you will be notified when the
+permissions have changed in any way, and if so, you will receive a link to
+review those diffs in the Metadelta UI!
+
+Hopefully this has been useful :)
